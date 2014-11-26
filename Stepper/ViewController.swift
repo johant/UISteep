aa4c3424980a23b68a2dc77322227872b7788c17
Johan Torroledo <jtorroledo@icloud.com>
2014-11-26 15:51:01 -0500

    First UISteeper EscualIT

--- Stepper/ViewController.swift
+++ Stepper/ViewController.swift
@@ -10,14 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var steepContar: UIStepper!
+    @IBOutlet weak var labelContador: UILabel!
+    
+    @IBAction func steeperValue(sender: UIStepper) {
+        labelContador.text = Int(sender.value).description
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        parametrizarLabel()
+        parametrizarSteep()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func parametrizarLabel(){
+        labelContador.text=""
+        labelContador.textColor = UIColor.blueColor()
+        
+        
+        
+    }
+    
+    func parametrizarSteep(){
+        steepContar.minimumValue = 1
+        steepContar.maximumValue = 100
+        steepContar.stepValue = 1
+        steepContar.autorepeat = true
+        
     }
 
 
